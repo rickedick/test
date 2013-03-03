c14b62ac4aef8edd043bbebacb4c0d15b0a28bf1
rickard <asdf@asdf.com>
2013-03-03 13:06:47 +0100

    hej

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-    $f = file_get_contents('http://tablaer.se');
+  
 
-  //  echo $f;
-
-  $version = curl_version();
-  echo '<pre>';
-      print_r($version);
-  echo '</pre>';
-  echo 'kuken';
-  echo 'ballaballa';
+ 
 ?>
+<h1>Hejsan svejsan</h1>
